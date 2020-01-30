1f535449e755b1eb5279783b4ffb51f4027d6ef8
Delacannon <delacannon@gmail.com>
2020-01-30 01:17:39 +0100

    added function

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,35 @@
-exports.handler = async event => {
-  const subject = event.queryStringParameters.name || "World";
+const nodemailer = require("nodemailer");
 
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}`
-  };
+exports.handler = function(event, context, callback) {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    port: 587,
+    secureConnection: false,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    },
+    tls: { ciphers: "SSLv3" }
+  });
+
+  console.log(event.body);
+
+  transporter.sendMail(
+    {
+      from: process.env.EMAIL,
+      to: "amat.alvar@gmail.com",
+      subject: "test lambda",
+      text: "Hola test from netlify lambda"
+    },
+    function(error, info) {
+      if (error) {
+        callback(error);
+      } else {
+        callback(null, {
+          statusCode: 200,
+          body: "Ok"
+        });
+      }
+    }
+  );
 };
